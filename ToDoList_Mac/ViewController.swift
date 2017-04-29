77f7b553a34b0cdcf0fa59cb4d1934b519b95579
Home-bl0b <B0nty-iMac27@b0ntys-imac.local>
2017-04-29 23:59:54 +0200

    Initial Commit

---
+++ ToDoList_Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ToDoList_Mac
+//
+//  Created by B0nty on 29/04/2017.
+//  Copyright © 2017 B0nty. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
